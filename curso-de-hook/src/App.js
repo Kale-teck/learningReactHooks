e4a85079840ef04a483ec2b5474c159b7dfbe462
Kalebe <kalebemartins220@gmail.com>
2023-05-07 18:13:02 -0300

    Pesquisa cep

---
+++ curso-de-hook/src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react'
+
+function App() {
+
+  const [endereco, setEndereco] = useState({})
+
+  function manipulaEndereco (evento) {
+
+    const cep = evento.target.value
+
+    setEndereco({
+      cep
+    })
+
+    if (cep && cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+          setEndereco(enderecoAntigo => ({
+            ...enderecoAntigo,
+            cep: dados.cep,
+            cidade: dados.localidade,
+            estado: dados.uf,
+            ddd: dados.ddd
+          }))
+        })
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input placeholder='Digite o CEP' onChange={manipulaEndereco}/>
+        <ul>
+          <li>
+            CEP: {endereco.cep}
+          </li>
+          <li>
+            Cidade: {endereco.cidade}
+          </li>
+          <li>
+            Estado: {endereco.estado}
+          </li>
+          <li>
+            DDD: {endereco.ddd}
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
